8af781a83b0dda08e7b2354f282a6e39f803c676
Ben Scholer <benscholer3248511@gmail.com>
2015-03-07 17:06:25 -0500

    Revert e3644a7..7b410e0
    This rolls back to commit e3644a7b9694faf3b78e8364dc9f017431cee4a6.

--- app/src/main/java/com/autohome/autohomeclient/Rooms.java
+++ app/src/main/java/com/autohome/autohomeclient/Rooms.java
@@ -1,64 +1,39 @@
 package com.autohome.autohomeclient;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.widget.Toolbar;
-
-import java.util.ArrayList;
-
-import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
-import it.gmariotti.cardslib.library.internal.CardHeader;
-import it.gmariotti.cardslib.library.view.CardListView;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 public class Rooms extends ActionBarActivity {
-
-	ArrayList<Card> cards = new ArrayList<>();
-	ArrayList<Room> rooms = new ArrayList<>();
-	CardListView cardView;
-	Room livingRoom = new Room("Living Room");
-	Room kitchen = new Room("Kitchen");
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_rooms);
-
-		rooms.add(livingRoom);
-		rooms.add(kitchen);
-		populate();
-
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		toolbar.setTitle("Rooms");
-
-		cardView = (CardListView) findViewById(R.id.card_list);
-
-		CardArrayAdapter cardArrayAdapter = new CardArrayAdapter(this, cards);
-		if (cardView != null) {
-			cardView.setAdapter(cardArrayAdapter);
-		}
 	}
 
-	public void populate() {
-		for (Room room : rooms) {
-			Card card = new Card(this);
-			card.setShadow(true);
 
-			CardHeader cardHeader = new CardHeader(this);
-			cardHeader.setTitle(room.getName());
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.menu_rooms, menu);
+		return true;
+	}
 
-			card.addCardHeader(cardHeader);
-			cards.add(card);
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
 		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
-
-
-
-
-
-
-
-
-
