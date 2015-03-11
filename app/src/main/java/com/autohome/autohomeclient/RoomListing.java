49769b2e9c71a7f822db49257c5d04977a38890b
Ben Scholer <benscholer3248511@gmail.com>
2015-03-11 17:03:07 -0400

    Cleanup code in RoomListing.java.

--- app/src/main/java/com/autohome/autohomeclient/RoomListing.java
+++ app/src/main/java/com/autohome/autohomeclient/RoomListing.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,7 +11,6 @@ import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
-
 
 public class RoomListing extends ActionBarActivity {
 
@@ -58,29 +55,5 @@ public class RoomListing extends ActionBarActivity {
 				startActivity(intent);
 			}
 		});
-
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.menu_rooms, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
 	}
 }
