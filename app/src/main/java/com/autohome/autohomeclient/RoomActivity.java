3323db3a935baa372834568d5f724300b7eb1e6f
Ben Scholer <benscholer3248511@gmail.com>
2015-03-12 14:22:25 -0400

    Fixed color of tabs.

--- app/src/main/java/com/autohome/autohomeclient/RoomActivity.java
+++ app/src/main/java/com/autohome/autohomeclient/RoomActivity.java
@@ -1,22 +1,24 @@
 package com.autohome.autohomeclient;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 
-import java.util.ArrayList;
-
-import it.gmariotti.cardslib.library.internal.Card;
-import it.gmariotti.cardslib.library.internal.CardGridArrayAdapter;
-import it.gmariotti.cardslib.library.view.CardGridView;
+import it.neokree.materialtabs.MaterialTab;
+import it.neokree.materialtabs.MaterialTabHost;
+import it.neokree.materialtabs.MaterialTabListener;
 
 
-public class RoomActivity extends ActionBarActivity {
+public class RoomActivity extends ActionBarActivity implements MaterialTabListener {
 
 	String roomName;
 	Toolbar toolbar;
-
+	MaterialTabHost tabHost;
+	ViewPager pager;
+	ViewPagerAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +30,79 @@ public class RoomActivity extends ActionBarActivity {
 			roomName = extras.getString(Shared.ROOM_NAME_INTENT_EXTRA_NAME);
 		}
 
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
 
+		tabHost = (MaterialTabHost) this.findViewById(R.id.room_tabHost);
+		pager = (ViewPager) this.findViewById(R.id.room_pager);
+
+		// init view pager
+		adapter = new ViewPagerAdapter(getSupportFragmentManager());
+		pager.setAdapter(adapter);
+		pager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+			@Override
+			public void onPageSelected(int position) {
+				// when user do a swipe the selected tab change
+				tabHost.setSelectedNavigationItem(position);
+
+			}
+		});
+
+		// insert all tabs from pagerAdapter data
+		for (int i = 0; i < adapter.getCount(); i++) {
+			tabHost.addTab(
+					tabHost.newTab()
+							.setText(adapter.getPageTitle(i))
+							.setTabListener(this)
+			);
+
+		}
 
 		toolbar.setTitle(roomName);
+	}
+
+
+	@Override
+	public void onTabSelected(MaterialTab tab) {
+		pager.setCurrentItem(tab.getPosition());
+	}
+
+	@Override
+	public void onTabReselected(MaterialTab tab) {
+
+	}
+
+	@Override
+	public void onTabUnselected(MaterialTab tab) {
+
+	}
+
+	private class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+		public ViewPagerAdapter(FragmentManager fm) {
+			super(fm);
+
+		}
+
+		public android.support.v4.app.Fragment getItem(int num) {
+			return new ApplianceListing();
+		}
+
+		@Override
+		public int getCount() {
+			return 2;
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position) {
+			switch (position) {
+				case 0:
+					return "Appliances";
+				case 1:
+					return "Units";
+				default:
+					return "";
+			}
+		}
+
 	}
 }
